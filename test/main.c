689e9e1cc414dd822c0ba37ace5328537d0d7bfe
Omar <omar.yassermarei@live.com>
2023-07-24 06:01:15 -0700

    Added conversion handling for %.

--- test/main.c
+++ test/main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    _printf("Let's try %co printf a %smple sentence.\n", 't', "si");
+    _printf("%%Let's try %co printf a %smple sentence.\n", 't', "si");
     printf("Let's try to printf a simple sentence.\n");
 
     return (0);
