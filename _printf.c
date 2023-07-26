45b097be8af9a72dca7e2dd2e1c41e2b5b4ba748
Omar <omar.yassermarei@live.com>
2023-07-25 17:31:13 -0700

    Edge cases,

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
